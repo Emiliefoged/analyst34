fdf338c104a033af80101606b9b6eb0a55ddaea6
Emiliefoged <s203742@dtu.dk>
2024-09-16 15:39:52 +0200

    Update StoreyRule.py

--- rules/StoreyRule.py
+++ rules/StoreyRule.py
@@ -1,9 +1,14 @@
 import ifcopenshell
 
-def checkRule2(model):
-    Walls = model.by_type('IfcWall')
+def checkRule(model):
+    levels = model.by_type('IfcBuildingStorey')
 
-    result = f"Walls: {len(Walls)}"
+    #Definer loftshøjde-grænsen i millimeter
+    elevation_threshold = 1500  # 1,5 meter er 1500 mm
+    #Filtrer etager baseret på loftshøjde
+    filtered_levels = [level for level in levels if level.Elevation is not None and level.Elevation > elevation_threshold]
+    # Print antallet af etager der opfylder kraven
+    result = f"Number of storeys including basements: {len(filtered_levels)}"
 
     return result
 
