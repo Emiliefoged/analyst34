7df252db29b17bdc8b8b9f39a2a970eb33909a88
Emiliefoged <s203742@dtu.dk>
2024-11-18 14:22:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import ifcopenshell
 
-from rules import windowRule
-from rules import doorRule
+from rules import GeometryRule
+from rules import GeometryRule3
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open("C:/Users/Emilie Foged Larsen/Dropbox/Pc/Desktop/DTU/Kandidat/BIM/CES_BLD_24_06_ARC.ifc")
+print("model loaded")
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+#volumeResult = GeometryRule.checkRule(model)
+#floorResult=GeometryRule1.checkRule(model)
+#floorResult=GeometryRule2.checkRule(model)
+floorResult=GeometryRule3.checkRule(model)
+
+
+#print("volume result:",volumeResult)
+#print("volume result:",floorResult)
+for result in floorResult:
+    print(result)
